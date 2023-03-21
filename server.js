812712a393832cdd6efe291645699000bc49694a
Ramiro Suarez <suarezramirof@gmail.com>
2023-03-20 21:53:20 -0300

    refactor: server.js

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ if (MODE === "CLUSTER" && cluster.isPrimary) {
 
   const app = express();
   app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
+  app.use(express.urlencoded({ extended: true }));
   app.use(express.static("./public"));
   app.use(cors());
 
