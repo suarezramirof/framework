e5fcb8d046eb1214f37978966a2b035139296dce
Ramiro Suarez <suarezramirof@gmail.com>
2023-03-20 22:47:32 -0300

    refactor: edit api url

--- public/js/main.js
+++ public/js/main.js
@@ -94,7 +94,7 @@ function updateMensajes(msjs) {
 // WebSocket
 
 socket.on(keys.nuevoProducto, () => {
-  fetch("http://localhost/api/productos")
+  fetch("https://process-production.up.railway.app/api/productos")
     .then((res) => res.json())
     .then((data) => {
       updateProductos({ items: data });
