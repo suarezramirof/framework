137bb69d378469c6f59eb380be9f868913d84e89
Ramiro Suarez <suarezramirof@gmail.com>
2023-03-22 21:46:44 -0300

    fix: Schema definition

--- src/models/ProductsSchema.js
+++ src/models/ProductsSchema.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const ProductsSchema = new mongoose.Schema({
   nombre: { type: String, required: true },
-  foto: { type: URL, required: true },
+  foto: { type: String, required: true },
   precio: {type: Number, required: true}
 });
 
