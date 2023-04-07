fd251282b50a1bfea6f599e48be7b6d5a085c042
Ramiro Suarez <suarezramirof@gmail.com>
2023-04-06 22:14:42 -0300

    refactor: remove file persistency

--- src/daos/MessagesFileDao.js
+++
@@ -1,9 +0,0 @@
-import FileContainer from "../containers/FileContainer.js";
-
-class MessagesFileDao extends FileContainer {
-  constructor() {
-    super("./DB/messages.json");
-  }
-}
-
-export default MessagesFileDao;
--- src/daos/ProductsFileDao.js
+++
@@ -1,9 +0,0 @@
-import FileContainer from "../containers/FileContainer.js";
-
-class ProductsFileDao extends FileContainer {
-  constructor() {
-    super("./DB/products.json");
-  }
-}
-
-export default ProductsFileDao;
