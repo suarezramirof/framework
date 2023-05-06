1524df701ecc693f2fc7bdc0e76e5544cbf02215
Ramiro Suarez <suarezramirof@gmail.com>
2023-05-06 17:05:51 -0300

    refactor: add method in products dao

--- src/daos/mongodb/ProductsMongoDao.js
+++ src/daos/mongodb/ProductsMongoDao.js
@@ -1,11 +1,34 @@
 import MongoDao from "./mongoDao.js";
 import ProductsSchema from "../../models/mongoose/productsSchema.js";
+import ProductsDto from "../dto/productsDto.js";
+import PinoLogger from "../../utils/logger.js";
+const consoleLogger = PinoLogger.buildConsoleLogger();
+const errorLogger = PinoLogger.buildErrorLogger();
 
 let instance = null;
 export default class ProductsMongoDao extends MongoDao {
   constructor() {
     super("products", ProductsSchema);
   }
+
+  async add(product) {
+    try {
+      if (product) {
+        const addTime = Date.now();
+        const productDto = new ProductsDto(null, product, addTime);
+        const { _id } = await this.items.create(productDto);
+        productDto._id = _id;
+        consoleLogger.info(
+          `Product  ${productDto.nombre} added with id: ${productDto._id}`
+        );
+        return productDto;
+      }
+      throw new Error("Must provide a product");
+    } catch (error) {
+      errorLogger.error("Error adding product: " + error);
+    }
+  }
+
   static getInstance() {
     if (!instance) {
       instance = new ProductsMongoDao();
