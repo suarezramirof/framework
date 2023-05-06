f3ba500f9bace7badf2b9e14830643fdcfb5292c
Ramiro Suarez <suarezramirof@gmail.com>
2023-05-06 17:03:51 -0300

    fix: id assignment

--- src/daos/dto/ProductsDto.js
+++ src/daos/dto/ProductsDto.js
@@ -1,6 +1,6 @@
 export default class ProductsDto {
   constructor(id, product, timestamp) {
-    this._id = id;
+    if (id) this._id = id;
     this.nombre = product.nombre;
     this.precio = product.precio;
     this.foto = product.foto;
